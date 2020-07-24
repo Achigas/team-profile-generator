0694921c6e4ca0a6ad63b5c3686a8f6a83c0cde5
AChigas <Achigas34@gmail.com>
2020-07-23 21:41:24 -0700

    employee tests written

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,16 +1,35 @@
 const Employee = require('../lib/Employee')
 
 test('creates an employee object', () => {
-    const employee = new Employee('Alex C',25,'Chig@email.com');
+    const employee = new Employee('Alex C','25','Chig@email.com');
     
     expect(employee.name).toBe('Alex C');
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.email).toEqual(expect.any(String))
+    expect(employee.id).toBe('25');
+    expect(employee.email).toBe('Chig@email.com')
   });
 
   test("gets employee's name", () => {
-    const employee = new Employee('Alex');
-    expect(employee.getName()).toHaveProperty('name');
+    const employee = new Employee('Alex','25','Chig@email.com');
+
+    expect(employee.getName()).toEqual(expect.stringContaining(employee.name));
+  });
+
+  test("gets employee's id", () => {
+    const employee = new Employee('Alex','25','Chig@email.com');
+
+    expect(employee.getId()).toEqual(expect.stringContaining(employee.id))
+  });
+
+  test("gets employee email", () => {
+    const employee = new Employee('Alex','25','Chig@email.com');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email));
+  });
+
+  test("gets employee role", () => {
+    const employee = new Employee('Alex','25','Chig@email.com');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.role));
   });
 
   module.exports = Employee 
